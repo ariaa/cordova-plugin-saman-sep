d8fed43e490fe89859ad64f5326e0916fa53ccf2
Aria Radmand <aria.radmand@gmail.com>
2019-02-02 16:05:41 +0330

    test method - android

--- src/android/net/ariaa/CordovaSamanSEPPlugin.java
+++ src/android/net/ariaa/CordovaSamanSEPPlugin.java
@@ -11,6 +11,7 @@ import org.apache.cordova.PluginResult.Status;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
+import ir.sep.Sdk724;
 
 import android.util.Log;
 
